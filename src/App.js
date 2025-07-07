45684168eef5a1c02006d92fbf3ef640aefe7890
ReneLouis <rg.louis@hotmail.fr>
2025-07-07 20:36:33 +0100

    feat: Add button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyButton from "./components/MyButton/MyButton";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MyButton />
     </div>
   );
 }
---
+++ src/components/MyButton/MyButton.js
@@ -0,0 +1,10 @@
+import "./MyButton.css";
+function MyButton() {
+  return (
+    <div className="Button">
+      <button>I'm a button</button>
+    </div>
+  );
+}
+
+export default MyButton;
