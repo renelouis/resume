7fe0cc0422ef7ae18efb9469c5cf6d6b79353e3c
ReneLouis <rg.louis@hotmail.fr>
2025-07-08 20:41:21 +0100

    feat: add profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyButton from "./components/MyButton/MyButton";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
+      <Profile />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Profile.css"; // Import the CSS file for styling
+
+const Profile = () => {
+  return (
+    <div className="Profile">
+      <div className="Profile-content">
+        <h2>Profile</h2>
+        {/* Add profile details here */}
+        <p>
+          Software engineer with experience in Java, Spring Boot, Kafka, GCP,
+          etc..
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
