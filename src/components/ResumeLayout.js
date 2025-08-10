99de17e56a90bdb82e6757113ccffd01cd4cbd99
ReneLouis <rg.louis@hotmail.fr>
2025-08-10 19:50:09 +0100

    fix: create new hook for header and remvove code from ResumeLayout.js

--- src/components/ResumeLayout.js
+++ src/components/ResumeLayout.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
+import { useResponsiveHeaderShrink } from "./skills/hooks/useResponsiveHeaderShrink";
 import "./ResumeLayout.scss";
 import NavBar from "./NavBar";
 
@@ -12,41 +13,9 @@ const navSections = [
 
 const ResumeLayout = ({ left, right, skills, personalInfo }) => {
   const headerRef = useRef(null);
-  const [showHamburger, setShowHamburger] = useState(
-    () => window.innerWidth <= 700
-  );
-  const [menuOpen, setMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (!headerRef.current) return;
-      if (window.scrollY > 30 || window.innerWidth <= 700) {
-        headerRef.current.classList.add("shrink");
-        setShowHamburger(true);
-      } else {
-        headerRef.current.classList.remove("shrink");
-        setShowHamburger(false);
-        setMenuOpen(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    // window.addEventListener("resize", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  // For mobile: always show hamburger, never NavBar
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 700) {
-        setShowHamburger(true);
-      } else if (window.scrollY <= 30) {
-        setShowHamburger(false);
-      }
-    };
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  const { isShrunk, showHamburger, menuOpen, setMenuOpen } =
+    useResponsiveHeaderShrink(headerRef);
 
   const handleMenuClick = (anchor) => {
     const el = document.getElementById(anchor);
@@ -95,6 +64,7 @@ const ResumeLayout = ({ left, right, skills, personalInfo }) => {
           <h2 className="resume-title">{personalInfo.jobTitle}</h2>
         </section>
       </header>
+      {/* <div style={{ height: isShrunk ? "100px" : "100px" }}></div> */}
       {/* Hide NavBar if hamburger is shown */}
       {!showHamburger && <NavBar />}
       <div className="resume-content">
